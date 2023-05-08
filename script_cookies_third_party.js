11180ad35dd28f10440f10caa71e592c1e4e23c6
joaovuolo <50891466+joaovuolo@users.noreply.github.com>
2023-05-08 13:42:36 +0200

    Add files via upload

---
+++ script_cookies_third_party.js
@@ -0,0 +1,9 @@
+if(document.querySelector('.home')  !== null){
+    document.getElementsByClassName('home')[0].innerHTML = "<div style='text-align:center;margin-bottom:15px;'><img width='50%' src='https://betterplace-assets.betterplace.org/uploads/project/profile_picture/000/118/495/crop_original_bc058750-34ae-443a-8818-45fe2412b67e.png'></div>" + document.getElementsByClassName('home')[0].innerHTML;
+}
+if (document.querySelector('.standard')  !== null){
+    document.getElementsByClassName('standard')[0].innerHTML = "<div style='text-align:center;margin-bottom:15px;'><img width='50%' src='https://betterplace-assets.betterplace.org/uploads/project/profile_picture/000/118/495/crop_original_bc058750-34ae-443a-8818-45fe2412b67e.png'></div>" + document.getElementsByClassName('standard')[0].innerHTML;
+}
+if(document.querySelector('.content')  !== null){
+    document.getElementsByClassName('content')[0].innerHTML = "<div style='text-align:center;margin-bottom:15px;'><img width='50%' src='https://betterplace-assets.betterplace.org/uploads/project/profile_picture/000/118/495/crop_original_bc058750-34ae-443a-8818-45fe2412b67e.png'></div>" + document.getElementsByClassName('content')[0].innerHTML;
+}
